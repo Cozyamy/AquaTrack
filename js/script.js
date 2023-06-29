f3aa9ee6d99ed6a1320798a5c40a518058b14896
Cozyamy <cozyamy@gmail.com>
2023-06-29 12:14:12 +0100

    dashboard page 1 ready for corrections

--- js/script.js
+++ js/script.js
@@ -13,21 +13,13 @@ addEventListener('click', () => {
 	navMenu.classList.remove('active');
 }))
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+const dropdownButton = document.querySelector('.dropdown-button');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+dropdownButton.addEventListener('click', function() {
+  dropdownContent.classList.toggle('show');
+});
+
 
 
 
