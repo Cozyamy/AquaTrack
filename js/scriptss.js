17074a7a20cbdcf80322cd6260dd840dd3d3a468
Amychy <amychyihejiako@gmail.com>
2023-08-02 10:03:48 +0100

    added the harvest page

--- js/scriptss.js
+++ js/scriptss.js
@@ -4,3 +4,65 @@ const dropdownContent = document.querySelector('.dropdown-content');
 dropdownButton.addEventListener('click', function() {
   dropdownContent.classList.toggle('show');
 });
+
+
+// for pay and paid toggle
+const paymentButtons = document.querySelectorAll(".pay");
+
+paymentButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    togglePaymentState(button);
+  });
+
+  // Set the initial text color based on the button text content
+  setButtonTextColor(button);
+});
+
+function togglePaymentState(button) {
+  const buttonText = button.textContent.trim();
+
+  // Toggle the button text and set the text color
+  if (buttonText === "Pay") {
+    button.textContent = "Paid";
+  } else if (buttonText === "Paid") {
+    button.textContent = "Pay";
+  }
+
+  setButtonTextColor(button);
+}
+
+function setButtonTextColor(button) {
+  const buttonText = button.textContent.trim();
+
+  // Add "paid" class to the button if it's "Paid", remove otherwise
+  if (buttonText === "Paid") {
+    button.classList.add("paid");
+  } else {
+    button.classList.remove("paid");
+  }
+}
+
+
+
+// for different tables harvest page
+const harvestOne = document.querySelector(".HarvestOne");
+    const harvestTwo = document.querySelector(".HarvestTwo");
+    const previousBtnOne = harvestOne.querySelector(".HarvestPrevious");
+    const nextBtnOne = harvestOne.querySelector(".HarvestNext");
+    const previousBtnTwo = harvestTwo.querySelector(".HarvestPrevious");
+    const nextBtnTwo = harvestTwo.querySelector(".HarvestNext");
+
+    previousBtnOne.addEventListener("click", () => {
+      harvestTwo.classList.remove("hidden"); // Show HarvestTwo
+      harvestOne.classList.add("hidden"); // Hide HarvestOne
+    });
+
+    nextBtnTwo.addEventListener("click", () => {
+      harvestOne.classList.remove("hidden"); // Show HarvestOne
+      harvestTwo.classList.add("hidden"); // Hide HarvestTwo
+    });
+
+
+
+
+
