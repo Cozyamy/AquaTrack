1fa74d78d0ad3c70cb2584ae60b812b7055b99ea
Amychy <amychyihejiako@gmail.com>
2023-08-03 23:37:41 +0100

    added the settings page

---
+++ js/settingscript.js
@@ -0,0 +1,15 @@
+const btn9 = document.getElementById("btn9");
+const btn10 = document.getElementById("btn10");
+
+// Add click event listeners to the buttons
+
+btn9.addEventListener("click", function() {
+  window.location.href = "settings.html";
+});
+
+btn10.addEventListener("click", function() {
+  window.location.href = "manage.html";
+});
+
+
+
